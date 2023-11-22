ee567e8a973a4dfcec7563b9582e23703e3ab83a
Rasmus <rasmustofthermansen@gmail.com>
2023-11-22 17:15:44 +0100

    porifle update 2

--- Navneguide/src/components/Profile.tsx
+++ Navneguide/src/components/Profile.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProfileCSS.css';
-function AddToList()
-{
-  
+
+interface UserData {
+  names: {
+    name: string;
+    gender: string;
+    isInternational: boolean;
+  }[];
+
 }
+
 function Profile() {
-  return ( 
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/users/655dfdd41948c9d6c4343703', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const data: UserData = await response.json();
+          setUserData(data);
+        } else {
+          console.error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
     <div className="profile-container">
+     
       <form className="profile-form">
         <div className="profile-group">
           <label htmlFor="UserName">Username:</label>
@@ -23,20 +56,23 @@ function Profile() {
         </button>  
       </form>
       <div className="list-container">
+      <h1>List</h1>
         <ul className="list">
-          <li>List Item 1</li>
-          <li>List Item 2</li>
-          <li>List Item 3</li>
+          {userData &&
+            userData.names.map((nameData, index) => (
+              <li key={index}>
+                Name: {nameData.name}, Gender: {nameData.gender}, International: {nameData.isInternational ? 'Yes' : 'No'}
+              </li>
+            ))}
         </ul>
       </div>
       <div className='add-more'>
-            <input id='ral' type="text"/>
-            <button id='add-button' className='add-button'>
-              Link youre partner
-            </button>
-        </div>
+        <input id='ral' type="text"/>
+        <button id='add-button' className='add-button'>
+          Link your partner
+        </button>
+      </div>
     </div>
-    
   );
 }
 
