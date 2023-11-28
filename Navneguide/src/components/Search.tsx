caa31b10bcfdbf0618792cc51f980d208c1851cf
Rasmus <rasmustofthermansen@gmail.com>
2023-11-28 11:41:02 +0100

    Henter nu partneren navne in i search

--- Navneguide/src/components/Search.tsx
+++ Navneguide/src/components/Search.tsx
@@ -1,62 +1,84 @@
-import './SearchCSS.css';
+import React, { useState, useEffect } from 'react';
+import './SearchCSS.css'; // Import your CSS file
+
 function Search() {
-    return(
-       <div className="search-container">
-         <div className='search-list'>
-          <h1>Partners navne</h1>
-         <ul className="list">
-          <li>List Item 1</li>
-          <li>List Item 2</li>
-          <li>List Item 3</li>
-          
+  const [names, setNames] = useState<string[]>([]);
+  const [partnerEmail, setPartnerEmail] = useState<string | null>(null);
+  const [linked, setLinked] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const partnerEmail = sessionStorage.getItem('partnerEmail');
+        const response = await fetch(`http://localhost:5000/users/email/${partnerEmail}`, {
+          method: 'GET',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          // Assuming data.names contains an array of names
+          setNames(data.names || []);
+        } else {
+          console.error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="search-container">
+      <div className='search-list'>
+        <h1>Partners navne</h1>
+        <ul className="pop-names-list">
+          {names.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className='search-list2'>
+        <h1>Match navne</h1>
+        
+        <ul className="list">
           
         </ul>
-         </div>
-         <div className='search-list2'>
-          <h1>Match navne</h1>
-         <ul className="list">
-          <li>List Item 1</li>
-          <li>List Item 2</li>
-          <li>List Item 3</li>
-          
-          
-          
-        </ul>
-         </div>
-            <form className="search-form">
-            <div className="search-group">
-            <label>Search:</label>
-            <input type="text" />
+      </div>
+      <form className="search-form">
+        
+        <div className="search-group">
+          <label>Search:</label>
+          <input type="text" />
           <div className="checkbox-container">
-          <div className="checkbox-div">
-            <label>
-              <input type="checkbox" id='checkbox1' name="checkbox1" />
-              Køn
-            </label>
+            <div className="checkbox-div">
+              <label>
+                <input type="checkbox" id='checkbox1' name="checkbox1" />
+                Køn
+              </label>
+            </div>
+            <div className="checkbox-div">
+              <label>
+                <input type="checkbox" id='checkbox2' name="checkbox2" />
+                Unisex
+              </label>
+            </div>
+            <div className="checkbox-div">
+              <label>
+                <input type="checkbox" id='checkbox3' name="checkbox3" />
+                International
+              </label>
+            </div>
           </div>
-
-          <div className="checkbox-div">
-            <label>
-              <input type="checkbox" id='checkbox2' name="checkbox2" />
-              Unisex
-            </label>
-          </div>
-
-          <div className="checkbox-div">
-            <label>
-              <input type="checkbox" id='checkbox3' name="checkbox3" />
-              International
-            </label>
-          </div>
-         
         </div>
+      </form>
+    </div>
+  );
+}
 
-
-        </div>
-            </form>
-       </div>
-    )
-
-  }
-  
-  export default Search
+export default Search;
