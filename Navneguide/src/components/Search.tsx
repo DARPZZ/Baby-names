d9b296add7e096331fdadee25ac6deceec2d35d0
Rasmus <rasmustofthermansen@gmail.com>
2023-12-04 13:24:45 +0100

    Tilføjet bootstrap

--- Navneguide/src/components/Search.tsx
+++ Navneguide/src/components/Search.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './SearchCSS.css'; 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Search() {
  
   const [names, setNames] = useState<string[]>([]);
@@ -162,75 +162,55 @@ function handleChange() {
     }   
   
 
-  return (
-    <div className="search-container">
-      <div className='search-list3'>
-        <table className="search-list3">
-          
+    return (
+      
+      <div className="container">
+        
+        <form className="form-inline">
+          <div className="form-group">
+            <label htmlFor="search-input">Search:</label>
+            <input onChange={handleSearch} id='search-input' type="text" className="form-control mx-sm-3" />
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id='mandCheckbox' name="checkbox1" onChange={handleChange} />
+            <label className="form-check-label" htmlFor="mandCheckbox">Mand</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id='kvindeCheckbox' name="checkbox1" onChange={handleChange} />
+            <label className="form-check-label" htmlFor="kvindeCheckbox">Kvinde</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id='unisexCheckbox' name="checkbox1" onChange={handleChange} />
+            <label className="form-check-label" htmlFor="unisexCheckbox">Unisex</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id='internationalCheckbox' name="checkbox1" onChange={handleChange} />
+            <label className="form-check-label" htmlFor="internationalCheckbox">International</label>
+          </div>
+        </form>
+        <table className="table table-striped table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Egne navne</th>
+              <th scope="col">Match navne</th>
+              <th scope="col">Partner navne</th>
+            </tr>
+          </thead>
           <tbody>
-          <tr>
-             <td>
-             <h5>Egne navne</h5>
-                <ul className="list">
-                  {namesArray.map((name, index) => (
-                    <li key={index}>{name}</li>
-                  ))}
-                </ul>
-              </td>
-              <td>
-                <h5>Match navne</h5>
-                <ul className="list">
-                {modificeeredeNavne.map((name, index) => (
-                    <li key={index}>{name}</li>
-                  ))}
-                </ul>
-              </td>
-              <td>
-              <h5>Partner navne</h5>
-                <ul className="pop-names-list">
-                  {names.map((name, index) => (
-                    <li key={index}>{name}</li>
-                  ))}
-                </ul>
-              </td>
-            </tr> 
+            {namesArray.map((name, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{namesArray[index]}</td>
+                <td>{modificeeredeNavne[index]}</td>
+                <td>{names[index]}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
+  
       </div>
-      <form className="search-form">
-        <div className="search-group">
-          <label>Search:</label>
-          <input onChange={handleSearch} id='search-input' type="text" />
-          <div className="checkbox-container">
-            <div className="checkbox-div">
-              <label>
-                <input type="checkbox" id='mandCheckbox' name="checkbox1" onChange={handleChange} />
-                Mand
-              </label>
-            </div>
-            <div className="checkbox-div">
-              <label>
-                <input type="checkbox" id='kvindeCheckbox' name="checkbox1" onChange={handleChange} />
-                Kvinde
-              </label>
-            </div>
-            <div className="checkbox-div">
-              <label>
-                <input type="checkbox" id='unisexCheckbox' name="checkbox1" onChange={handleChange} />
-                Unisex
-              </label>
-            </div>
-            <div className="checkbox-div">
-              <label>
-                <input type="checkbox" id='internationalCheckbox' name="checkbox1"onChange={handleChange} />
-                International
-              </label>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
-}
+    );
+  }
 
 export default Search;
