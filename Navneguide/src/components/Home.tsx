fcaa29cd23ed8c02a6282b3b98813eb60c4f7170
Rasmus <rasmustofthermansen@gmail.com>
2023-12-05 12:25:37 +0100

    Typewriter active

--- Navneguide/src/components/Home.tsx
+++ Navneguide/src/components/Home.tsx
@@ -1,8 +1,28 @@
 import './HomeCSS.css';
+import { Typewriter } from 'react-simple-typewriter'
+
 function Home() {
   return( 
+    
     <div className='home-page'>
+      <div className=' test'>
+        <h1 style={{ color: 'whitesmoke', paddingTop: '5rem', margin: 'auto 0', fontWeight: 'bold' }}>
+          We can help you with{' '}
+          <span style={{ color: 'red', fontWeight: 'bold' }}>
+            <Typewriter
+              words={['finding boys names', 'finding girls names', 'finding popular names', 'and much more!']}
+              loop={Infinity}
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={2000}
+            />
+          </span>
+        </h1>
+      </div>
       <div className='home-container'>
+        
             <img
                 className="LOGO"
                 src="https://cdn.discordapp.com/attachments/1060882642570072134/1181542272949563412/EBN_Logo.png?ex=65816ffd&is=656efafd&hm=810cada5243de0cb22e1547b225ff53043691a23fe21bd190a21bf678fb66fb2&"
