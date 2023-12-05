55881fffddefc605ac4f1f58af3f04c4c79d82c3
Rasmus <rasmustofthermansen@gmail.com>
2023-12-05 08:27:39 +0100

    Tilføjet svg til search

--- Navneguide/src/components/Search.tsx
+++ Navneguide/src/components/Search.tsx
@@ -167,10 +167,21 @@ function handleChange() {
       <div className="container">
         
         <form className="form-inline">
-          <div className="form-group">
-            <label htmlFor="search-input">Search:</label>
-            <input onChange={handleSearch} id='search-input' type="text" className="form-control mx-sm-3" />
+        <div className="form-group">
+          <label htmlFor="search-input">Search:</label>
+          <input 
+            onChange={handleSearch} 
+            id='search-input' 
+            type="text" 
+            className="form-control mx-sm-3" 
+            style={{
+              backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/></svg>')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'right'
+            }}
+          />
           </div>
+
           <div className="form-check form-check-inline">
             <input className="form-check-input" type="checkbox" id='mandCheckbox' name="checkbox1" onChange={handleChange} />
             <label className="form-check-label" htmlFor="mandCheckbox">Mand</label>
