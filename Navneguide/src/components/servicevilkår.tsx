11d4434a6bdf6bc487b1783b49aed8ce9c9b75b8
Rasmus <rasmustofthermansen@gmail.com>
2023-12-08 10:33:20 +0100

    Lavet vilkår og betingelser færdig

--- Navneguide/src/components/servicevilkår.tsx
+++ Navneguide/src/components/servicevilkår.tsx
@@ -16,7 +16,35 @@ function servicevilkår() {
              accepterer du at være bundet af disse Vilkår og Betingelser.
               Hvis du ikke er enig i nogen del af disse vilkår, må du ikke få adgang til webstedet.
             </p>
-            
+            <h3>
+            2. Brug af Tjenesten
+
+            </h3>
+            <p>
+                a. EasybabyNames giver en platform for forældre til at udforske og opdage babynavne.
+                 Brugere er udelukkende ansvarlige for de navne, de vælger, og EasybabyNames påtager sig intet ansvar for konsekvenserne af de valgte navne.
+                <br />
+                <br />
+                b. Brugere må ikke bruge hjemmesiden til ulovlige eller forbudte formål.
+                 Alle oplysninger, der gives, skal være nøjagtige.
+            </p>
+            <h3>
+            3. Fortrolighedspolitik
+            </h3>
+            <p>
+                a. EasybabyNames respekterer dit privatliv.
+                Ved at bruge hjemmesiden giver du samtykke til indsamling og brug af oplysninger.
+              </p>
+            <h3>
+            4. Kontaktoplysninger
+            </h3>
+            <p>
+                Hvis du har spørgsmål eller bekymringer vedrørende disse Vilkår og Betingelser, bedes du kontakte os på EasybabyNames@gmail.com.
+
+                Ved at bruge EasybabyNames accepterer du at overholde disse Vilkår og Betingelser. Tak fordi du bruger EasybabyNames
+
+
+            </p>
         </div>
     </div>
 )};
