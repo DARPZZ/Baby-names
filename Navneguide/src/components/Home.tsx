1b280d1925e15a6171c4cf64ccb2e77454d71c18
Rasmus <rasmustofthermansen@gmail.com>
2023-12-11 09:48:48 +0100

    Updated with rel pictures

--- Navneguide/src/components/Home.tsx
+++ Navneguide/src/components/Home.tsx
@@ -1,6 +1,6 @@
 import './HomeCSS.css';
 import { Typewriter } from 'react-simple-typewriter'
-
+import logo from './Billeder/EBN Logo.png';
 function Home() {
   return( 
     <div className='home-page'>
@@ -22,10 +22,8 @@ function Home() {
       </div>
       <div className='home-container'>
         
-            <img
-                className="LOGO"
-                src="https://cdn.discordapp.com/attachments/1060882642570072134/1181542272949563412/EBN_Logo.png?ex=65816ffd&is=656efafd&hm=810cada5243de0cb22e1547b225ff53043691a23fe21bd190a21bf678fb66fb2&"
-              />
+      <img className="LOGO" src={logo} />
+
       </div>
   </div> 
 )};
